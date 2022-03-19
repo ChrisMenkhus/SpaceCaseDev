eb30a70c4d9e33f0625faa24a7236930d9ee558c
Chris <menkhus.chris@gmail.com>
2022-03-19 01:35:18 -0600

    #74 Updated Redux actions

--- src/components/organisms/ArticleStyleCard/components/CardWrapper.tsx
+++ src/components/organisms/ArticleStyleCard/components/CardWrapper.tsx
@@ -4,7 +4,7 @@ type CardWrapperProps = HTMLAttributes<HTMLElement> & {}
 
 export function CardWrapper({ ...props }: CardWrapperProps) {
   return (
-    <article className="relative before:absolute before:-top-8 before:sm:-left-8 before:-z-10 z-10 mt-8 before:w-40 max-w-screen-lg before:h-40 before:bg-primary before:rounded-md before:shadow-md ">
+    <article className="relative mt-8 max-w-screen-lg ">
       <div className="flex overflow-hidden flex-col w-full max-w-screen-xl h-full text-black dark:text-white bg-white dark:bg-dark rounded shadow-lg md:flex-row">
         {props.children}
       </div>
